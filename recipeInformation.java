9049a49c56ac46330d9a6bf54cdc798ab5f1437c
sekiguchihiroki <sekiguchihiroki@sekiguchihiroki-no-MacBook-Pro.local>
2015-03-19 10:43:33 +0900

    spec3:ファイル分離

--- recipeInformation.java
+++ recipeInformation.java
@@ -1,5 +1,36 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
 class recipeInformation{
   public static void main(String args[]){
-    System.out.println("オムライス");
-  }
+
+ try {
+            //ファイルを読み込む
+            FileReader fr = new FileReader("recipe-data.txt");
+            BufferedReader br = new BufferedReader(fr);
+
+            //読み込んだファイルを１行ずつ処理する
+            String line;
+            StringTokenizer token;
+            while ((line = br.readLine()) != null) {
+                //区切り文字","で分割する
+                token = new StringTokenizer(line, ",");
+
+                //分割した文字を画面出力する
+                while (token.hasMoreTokens()) {
+                    System.out.println(token.nextToken());
+                }
+                //System.out.println("**********");
+            }
+
+            //終了処理
+            br.close();
+
+        } catch (IOException ex) {
+            //例外発生時処理
+            ex.printStackTrace();
+        } 
+   }
 }
